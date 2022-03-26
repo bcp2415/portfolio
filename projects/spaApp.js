3e7450ea7c1f420973e3e645bf85e29d201ad710
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2022-03-25 19:45:47 -0500

    adds basic bootstrap css and js files

---
+++ projects/spaApp.js
@@ -0,0 +1,33 @@
+// elements we want to act on when clicked:
+let techDescription = document.getElementById("techDescription");
+console.log(techDescription);
+const files = document.getElementById("files");
+const story = document.getElementById("story");
+const launch = document.getElementById("launch");
+const githubLink = document.getElementById("githubLink");
+
+// set up eventListeners
+techDescription.addEventListener("click", viewTechDetails);
+// files.addEventListener("click", viewFiles);
+// story.addEventListener("click", viewStory);
+// launch.addEventListener("click", launchApp);
+// githubLink.addEventListener("click", goToGithub);
+
+// other global variables to keep track of states
+let techDetails = false;
+
+function viewTechDetails() {
+  if (techDetails) {
+    techDescription.innerHTML = "technical description of the project";
+  }
+
+  if (!techDetails) {
+    techDescription.innerHTML = `technical description of the project<ul><li>Single-page App written in vanilla JavaScript</li><li>Stores data in private Google Sheet as back end</li><li>Log in with Google Identity Services</li><li>Authenticate / Authorize using JSON Web Tokens, automatically prompts user to re-authenticate at next API call if token has expired</li></ul>`;
+  }
+
+  techDetails = !techDetails;
+}
+
+function viewFiles() {
+  //files.innerHTML =
+}
